b4866bfb9e59ebe9a52f277af776eccb33236485
talhavawda <talhavawda.dev@gmail.com>
2021-06-16 21:00:21 +0200

    handling input instance
    - readInputFile()
    - printInputInstance()

--- Main.py
+++ Main.py
@@ -1,28 +1,34 @@
 from Algorithms import *
 
 
+"""Problem Constants"""
+SUBJECTS = [
+				"Home Language", "First Additional Language", "Mathematics", "Natural Science", "Social Science",
+				"Technology", "Economic and Management Science", "Life Orientation", "Arts and Culture"
+			]
+
+# Using List Comprehension to define the Lesson and Timeslot numbers
+LESSONS = [lesson for lesson in range(1, 56)]
+TIMESLOTS = [timeslot for timeslot in range(1, 56)]
+
+
 
 def readInputFile(fileName: str):
 
 	inputFile = open(fileName)
-	inputInstance = Input() # A Variable of type Input to represent the input obtained from the text file
 
-	inputInstance.numGr7Classes = int(inputFile.readline())
-	inputInstance.numGr9Classes = int(inputFile.readline())
-	inputInstance.numGr9Classes = int(inputFile.readline())
 
-	totalnumClasses = inputInstance.numGr7Classes + inputInstance.numGr8Classes + inputInstance.numGr9Classes
+	numGr7Classes = int(inputFile.readline())
+	numGr8Classes = int(inputFile.readline())
+	numGr9Classes = int(inputFile.readline())
 
-	while True:  # Get the next non-empty/non-blank line (to skip over any blank lines between the number of classes of each grade and th number of teachers )
+	totalnumClasses = numGr7Classes + numGr8Classes + numGr9Classes
+
+	while True:  # Get the next non-empty/non-blank line (to skip over any blank lines between the number of classes of each grade and the number of teachers )
 		lineString = inputFile.readline()
 		if lineString != "\n":
-			inputInstance.numTeachers = int(lineString)
+			numTeachers = int(lineString)
 			break
-
-
-	for c in range(totalnumClasses): # For each class
-		
-
 
 
 	"""
@@ -31,17 +37,108 @@ def readInputFile(fileName: str):
 		 and the value at [i, j] is the TeacherID of the teacher who teaches Subject j to Class i
 		
 		It can also be viewed as a list of lists with each sublist being a row in the table - each sublist representing a Class.
-		For sublist i, the value at index j is the teacher (numerical value) i who teaches Subject j to Class i
+		For sublist i, the value at index j is the teacher (numerical value - TeacherID) i who teaches Subject j to Class i
 		
 
 	"""
 	teachingTable = []
 
+	for Class in range(totalnumClasses): # For each Class (using Uppercase so as to not be mistaken for the 'class' keyword)
+		while True:  # Get the next non-empty/non-blank line (to skip over any blank lines between the number of teachers and the table itself)
+			teacherAllocationString = inputFile.readline()
+			if teacherAllocationString != "\n":
+				break
+
+		teacherAllocations = []  #the array of allocations of Teachers to Subjects for this Class
+
+		for Subject in range(9): #For each (of the 9) Subject
+			teacherAllocationString = teacherAllocationString.strip() # Remove leading and trailing whitespaces so that the beginning of the string is a numerical value representing a teacher
+
+			"""
+				Get the index of the first comma in the teacherAllocationString
+				In the input file, teachers are separated by commas 
+				
+				
+			"""
+			commaIndex = teacherAllocationString.find(",")
+
+			"""
+				If we are at the last subject (Subject == 8), then  there wont be a comma after the 
+				TeacherID for this subject in the string (commaIndex will be -1), so dont do any slicing - the TeacherID (as a string)
+				for this Subject is the remaining teacherAllocationsString
+	
+				Else (if not the last Subject, thus there is a comma between the TeacherID for this Subject and the next Subject)
+				then to get the TeacherID (as a string) for this Subject, slice from the beginning of the teacherAllocationsString till the character
+				before the comma
+			"""
+
+			if commaIndex == -1:
+				teacherIDString = teacherAllocationString
+			else:
+				teacherIDString = teacherAllocationString[:commaIndex]
+
+			teacherID = int(teacherIDString)
+			teacherAllocations.append(teacherID)
+			teacherAllocationString = teacherAllocationString[commaIndex + 1:]  # Remove the teacherID for this Subject from teacherAllocationString
+
+		teachingTable.append(teacherAllocations)
+
+	inputFile.close()
 
 
-
+	# A Variable of type Input to represent the input obtained from the text file
+	inputInstance = Input(teachingTable, numTeachers, numGr7Classes, numGr8Classes, numGr9Classes)
 
 	return inputInstance
+
+def printInputInstance(input: Input):
+
+
+	print("Number of classes in Grade 7: \t\t", input.numGr7Classes)
+	print("Number of classes in Grade 8: \t\t", input.numGr8Classes)
+	print("Number of classes in Grade 9: \t\t", input.numGr9Classes)
+	print("\nNumber of teachers teaching in Grades 7-9:\t", input.numTeachers)
+
+	print("\nTeacher Class-Subject Allocation Table:\n")
+
+	teachingTable = input.teachingTable
+
+	print("\nTeacher Class-Subject Allocation Table:\n")
+
+
+	#Print Column Headings (Subjects)
+
+	print("Subjects ->", end="\t\t\t\t")
+	headerStr = "======================"
+
+	#Subjects are represented as digits from 0 to 8 but will display as 1 to 9
+	for subject in range(1, 10):
+		print(subject, end="\t")
+		headerStr += "===="
+
+	print("\nClasses:")
+	print(headerStr)
+
+	classNames = []
+
+	for i in range(input.numGr7Classes):
+		className = "Grade 7 - Class " + str(i+1)
+		classNames.append(className)
+
+	for i in range(input.numGr8Classes):
+		className = "Grade 8 - Class " + str(i+1)
+		classNames.append(className)
+
+	for i in range(input.numGr9Classes):
+		className = "Grade 9 - Class " + str(i+1)
+		classNames.append(className)
+
+
+	for Class in range(input.numGr7Classes+input.numGr8Classes+input.numGr9Classes):
+		print(classNames[Class], end="\t|\t") # Row Heading
+		for Teacher in teachingTable[Class]:
+			print(Teacher, end="\t")
+		print()
 
 
 def main():
@@ -61,19 +158,25 @@ def main():
 	print("==============================================================================================================\n\n")
 
 
+	print("Subjects: ")
+	for i in range(len(SUBJECTS)):
+		# Subjects are represented as digits from 0 to 8 but will display as 1 to 9
+		print("\tSubject ", i+1, ": ", SUBJECTS[i], sep="")
+
+	print()
+
+
+	input = readInputFile("Input/easy.txt")
+	printInputInstance(input)
+
 	input1 = readInputFile("Input/SampleInput1.txt")
 	input2 = readInputFile("Input/SampleInput2.txt")
 	input3 = readInputFile("Input/SampleInput3.txt")
-
-
 	for input in [input1, input2, input3]:
-
 		populationSizeGA =
 		populationSizeCSA =
-
 		geneticAlgorithm = GeneticAlgorithm(input, populationSizeGA)
 		catSwarmAlgorithm = CatSwarmAlgorithm(input, populationSizeCSA)
-
 		for algorithm in [geneticAlgorithm, catSwarmAlgorithm]:
 			algorithm.solveTimetable()
 
