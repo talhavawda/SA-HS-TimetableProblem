c16ed3be756e6a4ec3eeb3c7d6d8687afbda0b02
Ahmad Jawaad Shah <ahmadj1801@gmail.com>
2021-06-20 00:24:05 +0200

    minor modification

--- Algorithms.py
+++ Algorithms.py
@@ -497,9 +497,10 @@ class GeneticAlgorithm(TimetableAlgorithm):
                 pos = gene.index(g)
                 subject = self.LESSON_SUBJECTS[pos]
                 subjectsAllocatedForClass.append(subject)
-            for s in range(len(subjectsAllocatedForClass)-2):
+            for s in range(len(subjectsAllocatedForClass) - 2):
                 # 3 consec periods of the same subject
-                if subjectsAllocatedForClass[s] == subjectsAllocatedForClass[s + 1] and subjectsAllocatedForClass[s] == subjectsAllocatedForClass[s + 2]:
+                if subjectsAllocatedForClass[s] == subjectsAllocatedForClass[s + 1] and subjectsAllocatedForClass[s] == \
+                        subjectsAllocatedForClass[s + 2]:
                     fitness -= 2
                 else:
                     continue
@@ -507,9 +508,9 @@ class GeneticAlgorithm(TimetableAlgorithm):
             # check if there is 2 periods of the same subjects in the same day[not consecutive]
             for s in range(len(subjectsAllocatedForClass)):
                 subject = subjectsAllocatedForClass[s]
-                for t in range(s+2, 11):
+                for t in range(s + 2, 11):
                     if subject == subjectsAllocatedForClass[t]:
-                        fitness -=1
+                        fitness -= 1
         print('Individual fitness = ', fitness)
         return fitness
 
@@ -663,7 +664,7 @@ class CatSwarmAlgorithm(TimetableAlgorithm):
 
             teacherClassAlloc = list(range(1, 56))
 
-			# rows = classes, cols= timeslots
+            # rows = classes, cols= timeslots
             new_allocation = [[0 for i in range(len(self.TIMESLOTS))] for j in range(self.totalNumClasses)]  # cat i
             for j in range(len(new_allocation[0])):
                 random.shuffle(teacherClassAlloc)
@@ -779,22 +780,20 @@ class CatSwarmAlgorithm(TimetableAlgorithm):
 
     def Change_Random(self, cat_copy):
         # auxilliary procedure, section 3.4.3
-		rand_col = random.randint(0,len( self.TIMESLOTS) -1)
+        rand_col = random.randint(0, len(self.TIMESLOTS) - 1)
 
-		for row in range(len(cat_copy)):
-			for col in range(len(cat_copy[row])):
-				if cat_copy[row][col] == self.global_best_cat[row][col] and (not col == rand_col):
-					cat_copy[row][col] = cat_copy[row][rand_col]
-					break # only do this once per class
+        for row in range(len(cat_copy)):
+            for col in range(len(cat_copy[row])):
+                if cat_copy[row][col] == self.global_best_cat[row][col] and (not col == rand_col):
+                    cat_copy[row][col] = cat_copy[row][rand_col]
+                    break  # only do this once per class
 
-		# swap
-		for row in range(len(cat_copy)):
-			for col in range(len(cat_copy[row])):
-				cat_copy[row][col] = self.global_best_cat[row][col]
+        # swap
+        for row in range(len(cat_copy)):
+            for col in range(len(cat_copy[row])):
+                cat_copy[row][col] = self.global_best_cat[row][col]
 
-		return  cat_copy
-
-
+        return cat_copy
 
     def Valid(self, current_cat):
         # check whether current cat is valid
